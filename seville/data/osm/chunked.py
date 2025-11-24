6f201092fa8916bd1cced1a169ed8f0d1c357d11
xnekul <94606205+xnekul@users.noreply.github.com>
2025-11-24 12:25:17 +0100

    Implement OSM for Seville

---
+++ seville/data/osm/chunked.py
@@ -0,0 +1,62 @@
+import seville.data.osm.osmconvert
+import os
+
+"""
+The purpose of this stage is to cut the OSM data into smaller chunks so we can process
+it more easily later on.
+"""
+
+def configure(context):
+    context.stage("data.spatial.municipalities")
+    context.stage("seville.data.osm.osmconvert")
+
+    context.config("processes")
+
+    context.config("data_path")
+    context.config("osm_path_seville", "osm/andalucia-latest.osm.pbf")
+
+def process_municipality(context, zone_id):
+    input_path = context.data("input_path")
+    local_path = context.data("local_path")
+
+    seville.data.osm.osmconvert.run(context, [input_path,
+        "-B={}".format("{}/{}.poly".format(local_path, zone_id)),
+        "-o={}".format("{}/{}.osm.pbf".format(local_path, zone_id))], cwd = local_path)
+    
+    return zone_id
+    
+def execute(context):
+    # Load zones and convert to polyfiles
+    df_zones = context.stage("data.spatial.municipalities")[["commune_id", "geometry"]]
+    df_zones = df_zones.to_crs("EPSG:4326")
+
+    for zone_id, geometry in df_zones.itertuples(index = False):
+        if not hasattr(geometry, "exterior"):
+            geometry = geometry.convex_hull
+
+        data = []
+        data.append("polyfile")
+        data.append("polygon")
+
+        for coordinate in geometry.exterior.coords:
+            data.append("    %e    %e" % coordinate[:2])
+
+        data.append("END")
+        data.append("END")
+
+        with open("{}/{}.poly".format(context.path(), zone_id), "w+") as f:
+            f.write("\n".join(data))
+    
+    # Cut into chunks
+    with context.progress(label = "Chunking OSM data ...", total = len(df_zones)) as progress:
+        with context.parallel({
+            "input_path": os.path.abspath("{}/{}".format(context.config("data_path"), context.config("osm_path_seville"))),
+            "local_path": context.path()
+        }) as parallel:
+            for item in parallel.imap(process_municipality, df_zones["commune_id"].values):
+                progress.update()
+                
+    return df_zones["commune_id"].values
+
+def validate(context):
+    return os.path.getsize("{}/{}".format(context.config("data_path"), context.config("osm_path_seville")))
