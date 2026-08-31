233d37da9ed632a6c8f1b0cf88d91e1de2c312a6
xnekul <94606205+xnekul@users.noreply.github.com>
2026-08-31 16:09:52 +0200

    filter secondary locations

--- asuncion/locations/secondary.py
+++ asuncion/locations/secondary.py
@@ -8,7 +8,29 @@ Yield education location candidates.
 def configure(context):
     context.stage("asuncion.data.work")
 
+SECONDARY_BUILDING_TYPES = ['COMERCIO', 'SERVICIO']
+
 
 def execute(context):
-    # TODO: filter out industry and agriculture locations
-    return context.stage("asuncion.data.work")
+    # Load data
+    df = context.stage("asuncion.data.work")
+
+    df = df[df['type_category'].isin(SECONDARY_BUILDING_TYPES)].copy()
+    df = df.rename(columns = { "building_id": "location_id" })
+
+    df['offers_shop'] = df['type_category'] == 'COMERCIO'
+    df['offers_leisure'] = df['type_category'] == 'SERVICIO'
+    df['offers_other'] = True
+
+    # Identifiers
+    df["location_id"] = np.arange(len(df))
+    df["location_id"] = "sec_" + df["location_id"].astype(str)
+
+    df["weight"] = df["employees"]
+
+
+
+    return df[[
+        "location_id", "weight", "commune_id", "iris_id", "geometry", "offers_shop", "offers_leisure", "offers_other"
+    ]]
+
